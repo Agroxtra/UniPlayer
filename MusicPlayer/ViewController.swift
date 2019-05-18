776d8c5fb8a0b595b3f09a331e4bcbb3e614813e
Martin Zörfuss <martin.zoerfuss@kabelplus.at>
2019-05-18 14:38:04 +0200

    Initial Commit

---
+++ MusicPlayer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MusicPlayer
+//
+//  Created by Martin Zörfuss on 18.05.19.
+//  Copyright © 2019 Martin Zörfuss. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
