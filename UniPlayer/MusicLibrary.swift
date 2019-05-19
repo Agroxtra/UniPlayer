17ed7243e8d8d5d0f04173c73bcc6a1d5dfcf0b1
Lukas Putz <lukas@putz.me>
2019-05-19 20:07:29 +0200

    added getting metadata of mp3

--- UniPlayer/MusicLibrary.swift
+++ UniPlayer/MusicLibrary.swift
@@ -7,6 +7,8 @@
 //
 
 import Foundation
+import UIKit
+import AVFoundation
 
 class MusicLibrary {
     static var library = [Music]()
@@ -17,4 +19,45 @@ struct Music {
     let artist : String?
     let url : URL
     let title : String
+    let artwork : UIImage?
 }
+
+extension Music{
+    init(path: URL){
+        
+        url = path
+        
+        let playerItem = AVPlayerItem(url: path)
+        let metadataList = playerItem.asset.metadata as! [AVMetadataItem]
+        
+        var titleName : String?
+        var artistName : String?
+        var artworkImage : UIImage?
+        
+        for item in metadataList {
+            
+            guard let key = item.commonKey?.rawValue, let value = item.value else{
+                continue
+            }
+            
+            switch key {
+            case "title" :
+               titleName = value as? String
+            case "artist":
+                artistName = value as? String
+            case "artwork" where value is Data : artworkImage = UIImage(data: value as! Data)
+            default:
+                continue
+            }
+        }
+        
+        if let t = titleName {
+            title = t
+        } else {
+            title = url.lastPathComponent
+        }
+        artist = artistName
+        artwork = artworkImage
+    }
+}
+
