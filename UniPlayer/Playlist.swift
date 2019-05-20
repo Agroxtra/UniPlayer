fa0814983b0a145a2e9d1931cb6211b33597f0a7
Lukas Putz <lukas@putz.me>
2019-05-20 18:18:10 +0200

    Add Playlist class

---
+++ UniPlayer/Playlist.swift
@@ -0,0 +1,14 @@
+//
+//  Playlist.swift
+//  UniPlayer
+//
+//  Created by Lukas Putz on 20.05.19.
+//  Copyright © 2019 Martin Zörfuss. All rights reserved.
+//
+
+import Foundation
+
+class Playlist{
+    private var list: [Song] = []
+    
+}
