8974fbd48024d9876cfee432cc458e083898fa4c
Lukas Putz <lukas@putz.me>
2019-05-30 21:02:57 +0200

    Add returning ytsearchitems

--- UniPlayer/YoutubeSearch.swift
+++ UniPlayer/YoutubeSearch.swift
@@ -9,7 +9,8 @@
 import Foundation
 
 class YoutubeSearch {
-    func search(searchString: String){
+    
+    func search(searchString: String, completion: @escaping (_ items: [YoutubeSearchItem]) -> Void){
         var urlcomps = URLComponents(string: "https://www.googleapis.com/youtube/v3/search")
         let queryItems = [URLQueryItem(name: "part", value: "snippet"),
                           URLQueryItem(name: "q", value: searchString),
@@ -30,12 +31,14 @@ class YoutubeSearch {
                 if let dataString = String(data: data, encoding: .utf8),
                    let dict = Utilities.convertStringToDictionary(text: dataString) {
                     let items = dict["items"] as! [[String: AnyObject]]
+                    var ytsearchitems = [YoutubeSearchItem]()
                     for item in items{
                         let id = item["id"] as! [String: AnyObject]
                         let snippet = item["snippet"] as! [String: AnyObject]
-                        
                         print("ID: \(id["videoId"]!)  title: \(snippet["title"]!)")
+                        ytsearchitems.append(YoutubeSearchItem(title: snippet["title"] as! String, fileName: id["videoId"] as! String))
                     }
+                    completion(ytsearchitems)
                 }
             }
         }
